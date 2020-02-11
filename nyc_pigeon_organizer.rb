1bcad1bf44d8655bc188d855725cbfae4b7d9eff
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-11 06:28:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+   names_as_key = {}
+  
+  data.each do |key1, inner_hash|
+    inner_hash.each do |key2, name_array|
+      name_array.each do |name|
+        if !names_as_key.has_key?(name)
+          names_as_key[name] = {}
+        end
+        
+        if !names_as_key[name].has_key?(key1)
+          names_as_key[name][key1] = []
+        end
+        
+        if !names_as_key[name][key1].include?(key2)
+          names_as_key[name][key1] << key2.to_s
+        end
+      
+    
+      end
+    end  
+  end
+  names_as_key
+  
+  
 end
